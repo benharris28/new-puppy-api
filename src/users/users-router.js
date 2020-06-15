959b97e8b2f8efb95b681f3f1d5afb549bf3deb6
Ben Harris <benjaminharris28@gmail.com>
2020-06-15 16:22:33 -0400

    added post login

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -19,5 +19,72 @@ usersRouter
     })
     .catch(next)
 })
+.post(jsonBodyParser, (req, res, next) => {
+    const { email, password } = req.body
+
+    for (const field of ['email', 'password'])
+        if (!req.body[field])
+            return res.status(400).json({
+                error: `Missing '${field}' in request body`
+            })
+    
+    const passwordError = UsersService.validatePassword(password)
+
+    if (passwordError)
+        return res.status(400).json({ error: passwordError })
+
+    UsersService.hasUserWithEmail(
+        req.app.get('db'),
+        email
+    )
+    .then(hasUserWithEmail => {
+        if(hasUserWithEmail) 
+            return res.status(400).json({ error: `Username already taken`})
+        
+            
+    
+
+    return UsersService.hashPassword(password)
+        .then(hashedPassword => {
+            const newUser = {
+                email,
+                password: hashedPassword,
+                date_created: 'now()'
+            }
+        
+    
+    return UsersService.insertUser(
+        req.app.get('db'),
+        newUser
+    )
+    .then(user => {
+        console.log(user)
+        res
+            .status(201)
+            .location(path.posix.join(req.originalUrl, `/${user.id}`))
+            .json(UsersService.serializeUser(user))
+        }) 
+    })
+})
+    .catch(next) 
+
+})
+
+usersRouter
+.route('/:id')
+.patch(jsonBodyParser, (req, res, next) => {
+    const { product_complete, food_complete, housetrain_complete, feeding_complete, tricks_complete, pickup_complete, prephome_complete } = req.body;
+    const userUpdate = { product_complete, food_complete, housetrain_complete, feeding_complete, tricks_complete, pickup_complete, prephome_complete }
+    
+    UsersService.markComplete(
+        req.app.get('db'),
+        req.params.id,
+        userUpdate
+    )
+    .then(numRowsAffected => {
+        res.status(204).end()
+    })
+    .catch(next)
+})
 
 module.exports = usersRouter;
