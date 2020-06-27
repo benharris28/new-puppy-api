c2a0c4a173b9d3e9e20ac0fbc9e74f606f6fdea8
Ben Harris <benjaminharris28@gmail.com>
2020-06-27 15:17:12 -0400

    built out working query in map service

--- src/map/map-service.js
+++ src/map/map-service.js
@@ -1,19 +1,29 @@
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 
 const MapService = {
-    getMarkers(db, lat, lng) {
+    async getMarkers(db, lat, lng) {
         
-        return db
-            .from('markers')
-            .select(
-                'marker_id',
-                db.raw( 
-                    `(3959 * acos( cos( radians(37) ) * cos( radians( ${lat} ) ) * cos( radians( ${lng} ) - radians(-122) ) + sin( radians(37) ) * sin( radians( ${lat} ) ) ) ) AS distance`
-                ),
-            )
-            .having('distance', '<', 25)
-            .orderBy('distance')
-            .limit(5)
+        let query = await db.raw( 
+                    `select marker_id, distance
+                    from markers
+                    join (
+                    SELECT marker_id as id,
+                    (3959 * acos( cos( radians(${lat}) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(${lng}) ) + sin( radians(${lat}) ) * sin( radians( lat ) ) ) ) AS distance
+                    FROM markers) as s on s.id = markers.marker_id and distance < 10
+                    ORDER BY distance 
+                    LIMIT 20;`
+                )
+
+                return query.rows
+               
+            
+            
+
+
+
+         
+
+   
 
 
 
